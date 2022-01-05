effdfb623f2563b080864efa8dd21332d901a3a4
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2022-01-06 01:07:33 +0900

    The first release

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # url2emoji
-Convert Twemoji URL to emoji text.
+
+Convert URL of Twemoji image on twitter.com to emoji text.
+
+```typescript
+const emoji_svg = url_to_emoji("https://abs-0.twimg.com/emoji/v2/svg/1f600.svg");
+const emoji_png = url_to_emoji("https://abs-0.twimg.com/emoji/v2/72x72/1f600.png");
+console.log(emoji_svg); // 😀
+console.log(emoji_png); // 😀
+```
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+/**
+ * Convert url of Twemoji image to emoji. If given url is not valid format, return null.
+ * @param url url of the image
+ * @returns emoji
+ * @example ```typescript
+ * const emoji_svg = url_to_emoji("https://abs-0.twimg.com/emoji/v2/svg/1f600.svg");
+ * const emoji_png = url_to_emoji("https://abs-0.twimg.com/emoji/v2/72x72/1f600.png");
+ * console.log(emoji_svg); // 😀
+ * console.log(emoji_png); // 😀
+ * ```
+ */
+const url2emoji = (url: string) => {
+    const svg_emoji_url = /https:\/\/abs-0\.twimg\.com\/emoji\/v2\/svg\/(?<code_point>[\w-]+?)\.svg/u;
+    const png_emoji_url = /https:\/\/abs-0\.twimg\.com\/emoji\/v2\/72x72\/(?<code_point>[\w-]+?)\.png/u;
+
+    let code_point = null;
+
+    if (url.match(svg_emoji_url)) code_point = url.replace(svg_emoji_url, "$<code_point>");
+    else if (url.match(png_emoji_url)) code_point = url.replace(png_emoji_url, "$<code_point>");
+
+    if (!code_point) return null;
+
+    try {
+        const emoji = code_point
+            .split("-")
+            .map((txt) => String.fromCodePoint(parseInt(`0x${txt}`, 16)))
+            .join("");
+        return emoji;
+    } catch {
+        return null;
+    }
+};
+
+export { url2emoji };
